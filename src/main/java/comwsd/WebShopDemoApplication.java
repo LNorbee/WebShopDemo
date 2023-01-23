6133cecf46af10ab510b7556185cde715c59d490
LNorbee <lazar.norbeert@gmail.com>
2023-01-23 11:05:11 +0100

    Database connecting
    - set MySQL db in application.properties

---
+++ src/main/java/comwsd/WebShopDemoApplication.java
@@ -0,0 +1,13 @@
+package comwsd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebShopDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebShopDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/comwsd/WebShopDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package comwsd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebShopDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
